ff2d72c9ed8e6d1a70f7def075fa21b698159c56
Valithor Obsidion <valzargaming@gmail.com>
2020-05-27 16:14:30 -0400

    Automatic restarts, restcord
    Restcord is not used anywhere as of this commit

--- run.php
+++ run.php
@@ -30,6 +30,10 @@
 	$loop = \React\EventLoop\Factory::create();
 	$discord = new \CharlotteDunois\Yasmin\Client(array(), $loop);
 	echo PHP_EOL;
+	
+	use RestCord\DiscordClient;
+	$restcord = new DiscordClient(['token' => "{$token}"]); // Token is required
+	//var_dump($restcord->guild->getGuild(['guild.id' => 116927365652807686]));
 
 	/*
 	set_exception_handler(function (Throwable $e) {
@@ -41,7 +45,7 @@ try {
 		echo "[ERROR] $error" . PHP_EOL;
 	});
 
-	$discord->once('ready', function() use ($discord, $loop, $token){	// Listen for events here
+	$discord->once('ready', function() use ($discord, $loop, $token, $restcord){	// Listen for events here
 		echo "[SETUP]" . PHP_EOL;
 		//$line_count = COUNT(FILE(basename($_SERVER['PHP_SELF']))); //No longer relevant due to includes
 		$version = "RC V1.4.0";
@@ -50,15 +54,15 @@ try {
 			array(
 				'since' => null, //unix time (in milliseconds) of when the client went idle, or null if the client is not idle
 				'game' => array(
-				'name' => "$line_count lines of code! $version",
-					'name' => $version,
-					'type' => 3, //0, 1, 2, 3, 4 | Game/Playing, Streaming, Listening, Watching, Custom Status
-					'url' => null //stream url, is validated when type is 1, only Youtube and Twitch allowed
-					/*
-					Bots are only able to send name, type, and optionally url.
-					As bots cannot send states or emojis, they can't make effective use of custom statuses.
-					The header for a "Custom Status" may show up on their profile, but there is no actual custom status, because those fields are ignored.
-					*/
+				//'name' => "$line_count lines of code! $version",
+				'name' => $version,
+				'type' => 3, //0, 1, 2, 3, 4 | Game/Playing, Streaming, Listening, Watching, Custom Status
+				'url' => null //stream url, is validated when type is 1, only Youtube and Twitch allowed
+				/*
+				Bots are only able to send name, type, and optionally url.
+				As bots cannot send states or emojis, they can't make effective use of custom statuses.
+				The header for a "Custom Status" may show up on their profile, but there is no actual custom status, because those fields are ignored.
+				*/
 				),
 				'status' => 'dnd', //online, dnd, idle, invisible, offline
 				'afk' => false
@@ -72,7 +76,7 @@ try {
 		$dt = new DateTime("now"); // convert UNIX timestamp to PHP DateTime
 		echo $dt->format('d-m-Y H:i:s') . PHP_EOL; // output = 2017-01-01 00:00:00
 		
-		$discord->on('message', function($message) use ($discord, $loop, $token){ //Handling of a message
+		$discord->on('message', function($message) use ($discord, $loop, $token, $restcord){ //Handling of a message
 			include "message-include.php";
 		});
 			
@@ -189,11 +193,17 @@ try {
 	});
 
 	set_error_handler(function(int $number, string $message) {
-	   echo "Handler captured error $number: '$message'" . PHP_EOL  ;
+		if ($message != "Undefined variable: suggestion_pending_channel") //Expected to be null
+			echo "Handler captured error $number: '$message'" . PHP_EOL  ;
 	});
 	$discord->login($token)->done();
 	$loop->run();
 }catch (Throwable $e){
     echo "Captured Throwable: " . $e->getMessage() . PHP_EOL;
+	//Restart the bot if this gets called\
+	sleep(30);
+	echo "RESTARTING BOT" . PHP_EOL;
+	$restart_cmd = 'cmd /c "'. __DIR__  . '\run.bat"'; //echo $restart_cmd . PHP_EOL;
+	system($restart_cmd);
 }
 ?> 
